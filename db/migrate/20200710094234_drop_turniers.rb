db82b7269be5a944974d33615062abd1fe07c509
Rolf <rolf.zanga@acm.org>
2020-07-25 16:12:35 +0200

    Deleted  db migrations  for turniers, teilnehmers

--- db/migrate/20200710094234_drop_turniers.rb
+++
@@ -1,9 +0,0 @@
-class DropTurniers < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :turniers
-  end
-
-  def down
-    fail ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20200710094246_drop_teilnehmers.rb
+++
@@ -1,9 +0,0 @@
-class DropTeilnehmers < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :teilnehmers
-  end
-
-  def down
-    fail ActiveRecord::IrreversibleMigration
-  end
-end
