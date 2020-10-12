a1cb7cfaec77468c9b04dfdf74201241480f5a8d
Rolf <rolf.zanga@acm.org>
2020-10-12 09:25:52 +0200

    Expand CORS setting to allow other clients on LAN

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
+    origins 'http://localhost:8080', 'http://0.0.0.0:8080',
+            /http:\/\/192\.168\.\d{1,3}\.\d{1,3}(:\d+)?\/?/
 
     resource '*',
       headers: :any,
